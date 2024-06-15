b012287f352ca66c52471e1fee358faf1f02bc77
Conor <cyuen10@gmail.com>
2024-06-15 00:22:56 -0400

    modifications to eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,6 +14,8 @@ const config = {
     "plugin:@typescript-eslint/stylistic-type-checked"
   ],
   "rules": {
+    "eslint-disable-next-line @typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/prefer-nullish-coalescing": "off",
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
     "@typescript-eslint/consistent-type-imports": [
