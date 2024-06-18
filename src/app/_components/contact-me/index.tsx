2c20306b6d718546762942b0743e939ea53540c3
Conor <cyuen10@gmail.com>
2024-06-18 00:42:51 -0400

    Added message sent state to contact form button

--- src/app/_components/contact-me/index.tsx
+++ src/app/_components/contact-me/index.tsx
@@ -14,12 +14,17 @@ export default function ContactMe() {
   const { register, control, handleSubmit } = useForm();
   const sendMessage = api.message.send.useMutation();
   const [isLoading, setIsLoading] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
 
   //eslint-disable-next-line @typescript-eslint/no-explicit-any
   async function onSubmit(data: any) {
     setIsLoading(true);
     try {
-      const response = await sendMessage.mutateAsync(data)
+      await sendMessage.mutateAsync(data)
+      setIsSuccess(true)
+      setTimeout(() => {
+        setIsSuccess(false)
+      }, 2000)
     } catch (error) {
       console.error(error);
     }
@@ -33,7 +38,7 @@ export default function ContactMe() {
         <motion.div {...anim(opacity)}>
           <form onSubmit={(handleSubmit(onSubmit))} className="flex flex-col gap-y-2 max-w-[500px]">
             <h1 className={`${kaisei.className} text-5xl md:text-7xl font-bold mb-2`}>Let&apos;s work together!</h1>
-            <p className={`font-light mb-4`}>Email me at <a href="mailto:yuenc2@tcnj.edu" target="_blank" rel="noreferrer" className="underline hover:cursor-pointer">yuenc2@tcnj.edu</a> or drop a message below.</p>
+            <p className={`font-light mb-4`}>Email me at yuenc2@tcnj.edu or drop a message below.</p>
             <div className="flex gap-x-2">
               <Controller
                 name="name" 
@@ -85,8 +90,8 @@ export default function ContactMe() {
               )}
             />
             <div className="w-full flex justify-end">
-              <button type="submit" className="text-sm py-1.5 px-4 border border-slate-200 dark:border-slate-700 dark:bg-slate-800 rounded-md">
-                {isLoading ? <BeatLoader loading={isLoading} color="#06b6d4" size={4} /> : 'Send'}
+              <button type="submit" className="success text-sm py-1.5 px-4 border border-slate-200 dark:border-slate-700 dark:bg-slate-800 rounded-md">
+                {isSuccess ? 'Message sent! 🎉' : isLoading ? <BeatLoader loading={isLoading} color="#06b6d4" size={4} /> : 'Send'}
               </button>
             </div>
           </form>
